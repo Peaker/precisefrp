e6704aaaf614cddf4ca86bdfdb0bdd1ba25ea713
Eyal Lotem <eyal.lotem@gmail.com>
2009-10-07 23:24:30 +0200

    *broken* added a graph example and a Show instance

--- frp.hs
+++ frp.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE GADTs, KindSignatures #-}
 
-import Prelude hiding (fst, snd, id)
+import Prelude hiding (fst, snd, id, (.))
 import Control.Category(Category(..), (>>>))
 import Control.Arrow(Arrow(..))
+import Control.Concurrent(forkIO, killThread, threadDelay)
+import Control.Exception(bracket)
+import Control.Monad(forever)
 
 type Source a = SF () a
 -- type Sink a = SF a ()
@@ -23,7 +26,18 @@ data SF :: * -> * -> * where
   PureFunction :: (a -> b) -> SF a b
 
   IOInput :: IO a -> -- ^ a blocking action that yields the next input
-             Source a
+             SF () a
+
+instance Show (SF a b) where
+  show (SequentialCompose x y) = "(SequentialCompose " ++ show x ++ " " ++ show y ++ ")"
+  show (ParallelCompose x y) = "(ParallelCompose " ++ show x ++ " " ++ show y ++ ")"
+  show Identity = "Identity"
+  show Fst = "Fst"
+  show Snd = "Snd"
+  show Split = "Split"
+  show (Scanl initial func) = "Scanl"
+  show (PureFunction func) = "PureFunction"
+  show (IOInput action) = "IOInput"
 
 swap :: SF (a, b) (b, a)
 swap = Snd &&& Fst
@@ -55,10 +69,49 @@ scanlE :: s -> -- ^ initial state
           SF a s
 scanlE = Scanl
 
-execute :: SF () () -> IO ()
-execute = do
-  
+-- ^ Forks a thread and ensures thread is killed when block is left
+withFork :: IO () -- ^ Thread action
+         -> IO a -- ^ Block during which thread should run forked
+         -> IO a
+withFork act block = bracket (forkIO act) killThread (const block)
 
--- Arrow problems:
--- 1. Build-up and unpacking of tuples
--- 2. Over-computation: (a, b) only a changes, stuff that read b also get recomputed <-- solvable
+composition :: [a -> a] -> (a -> a)
+composition = foldr (.) id
+
+waitForever :: IO ()
+waitForever = forever $ threadDelay maxBound
+
+-- example graph
+a = id
+b = id
+c = id
+d = id
+e = id
+f = id
+g = id
+graph = ((a >>> b) *** (c >>> d)) >>> split >>> (e *** (snd >>> f)) >>> g
+--
+
+-- execute :: SF () () -> IO ()
+-- execute sf = do
+--   externalInputs <- buildGraph sf
+--   curState <- newState
+--   composition (map (withExternalInput curState) externalInputs) $ do
+--     waitForever
+--   where
+--     withExternalInput curState ei =
+--       withFork $ do
+--         updateState curState
+
+--   SequentialCompose :: SF input middle -> SF middle output -> SF input output
+--   ParallelCompose :: SF ai ao -> SF bi bo -> SF (ai, bi) (ao, bo)
+--   Identity :: SF a a
+--   Fst :: SF (a, b) a
+--   Snd :: SF (a, b) b
+--   Split :: SF a (a, a)
+--   Scanl :: s -> (s -> a -> s) -> SF a s
+--   -- blackbox: Can't determine what subset of its input it uses:
+--   PureFunction :: (a -> b) -> SF a b
+
+--   IOInput :: IO a -> -- ^ a blocking action that yields the next input
+--              Source a
